46ce6ce66a155e7109cc5cf8fe05902489290729
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-18 14:11:42 -0300

    Add project files.

---
+++ Desafio4.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Desafio4.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
