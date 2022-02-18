9c22e4e2edcf3ee20463de63e84a4d872c18529f
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-18 14:58:32 -0300

    comecei a declarar os numeros que serão adicionados aos vetores

--- Desafio5.ConsoleApp/Program.cs
+++ Desafio5.ConsoleApp/Program.cs
@@ -6,7 +6,19 @@ namespace Desafio5.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x;
+
+            Console.Write("Digite a quantidade de valores lidos: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            int[] numeros = new int[x];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write("Digite o valor da posição lidos: ");
+                x = Convert.ToInt32(Console.ReadLine());
+            }
+                    
         }
     }
 }
